2f90d16d9ea86b2017955e42df5da808961748b8
Kostin Pavel <kostin.pavel@hotmail.com>
2016-11-26 02:10:53 +0200

    created the encryption function with a plug on the server

---
+++ CaesarCipher/Controllers/EncryptController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaesarCipher.Controllers
+{
+    public class EncryptController : Controller
+    {
+        [HttpPost]
+        public JsonResult GetEncryption(string userText, int rotate)
+        {
+            var result = userText.ToUpper() + " - " + rotate;
+            return Json(result);
+        }
+
+    }
+}
